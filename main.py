10f3cf9633eaf72765c4ddca5701cc32bc191b57
adofai07 <chldmsgh695@gmail.com>
2024-08-03 18:32:32 +0900

    asdf

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 from engine import fight
 
-if __name__ == "__main__":
-    for i in range(1, 161, 4):
-        print(F"![](examples/board_example_{i}.png)|![](examples/board_example_{i + 2 - 1}.png)|![](examples/board_example_{i + 3 - 1}.png)|![](examples/board_example_{i + 4 - 1}.png)")
-        
-    # fight(
-    #     white="pnbqkr",
-    #     black="pnbrqk"
-    # )
+if __name__ == "__main__":        
+    fight(
+        white="pnbqkr",
+        black="pnbrqk"
+    )
